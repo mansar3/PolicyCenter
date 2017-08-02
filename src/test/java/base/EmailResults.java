8ef1c53c2175e2941e346ffd2781a9a361573bc6
aansari <aansari@flhi.com>
2017-08-02 16:38:37 -0400

    Whensafe dp3 bug fix.

--- src/test/java/base/EmailResults.java
+++ src/test/java/base/EmailResults.java
@@ -12,12 +12,7 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import javax.swing.filechooser.FileSystemView;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.Properties;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 /**
  * Created by ajmac on 4/25/17.
@@ -129,23 +124,22 @@ class EmailResults extends BaseTest{
 				System.out.println("~~~~~~~~~~~~~~~~~~~~~	Zipping ScreenShots	  ~~~~~~~~~~~~~~~~~~~~~~~~~~");
 				ZipUtil.pack(new File(filePathScreens), zippedScreensFile);
 				System.out.println("~~~~~~~~~~~~~~~~~~~~~	Screenshots have been zipped	~~~~~~~~~~~~~~~~~~~~~~~~~~");
-//				if(zippedScreensFile.length()/(Math.pow(1024,2)) < 25)
-//				{
-//
-//				}
-//                // now create this segment
-//                // First attachment here (Screens)
-//                MimeBodyPart attachmentBodyPartScreens = new MimeBodyPart();
-//
-//                String fileName = filePath + ".zip";
-//                DataSource source = new FileDataSource(zippedScreensFile);
-//                attachmentBodyPartScreens.setDataHandler(new DataHandler(source));
-//                attachmentBodyPartScreens.setFileName(fileName);
-//
-//                //add the attachment
-//
-//                multipart.addBodyPart(attachmentBodyPartScreens);
-//				System.out.println("Screenshots Attached");
+				if(!prod)
+				{
+					// now create this segment
+					// First attachment here (Screens)
+					MimeBodyPart attachmentBodyPartScreens = new MimeBodyPart();
+
+					String fileName = filePath + ".zip";
+					DataSource source = new FileDataSource(zippedScreensFile);
+					attachmentBodyPartScreens.setDataHandler(new DataHandler(source));
+					attachmentBodyPartScreens.setFileName(fileName);
+
+					//add the attachment
+
+					multipart.addBodyPart(attachmentBodyPartScreens);
+					System.out.println("Screenshots Attached");
+				}
 			}
 
 
@@ -167,33 +161,33 @@ class EmailResults extends BaseTest{
 			}
 
 			// Third attachment log file
-			if(!prod)
-			{
-				File logFile = new File("log.txt");
-				if(logFile.exists())
-				{
-					System.out.println("Log file found");
-					File zippedLogFile = new File("log.zip");
-					ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zippedLogFile));
-					ZipEntry e = new ZipEntry("log.txt");
-					out.putNextEntry(e);
-					out.closeEntry();
-					out.close();
-					String filePathLog = "log.zip";
-					//ZipUtil.pack(new File(filePathLog), zippedLogFile);
-					MimeBodyPart attachmentBodyPartLog = new MimeBodyPart();
-					String fileNameLog = "log.zip";
-
-
-					DataSource sourceLog = new FileDataSource(filePathLog);
-					attachmentBodyPartLog.setDataHandler(new DataHandler(sourceLog));
-					attachmentBodyPartLog.setFileName(fileNameLog);
-
-					// add to our message
-					multipart.addBodyPart(attachmentBodyPartLog);
-					System.out.println("Log File Attached");
-				}
-			}
+//			if(!prod)
+//			{
+//				File logFile = new File("log.txt");
+//				if(logFile.exists())
+//				{
+//					System.out.println("Log file found");
+//					File zippedLogFile = new File("log.zip");
+//					ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zippedLogFile));
+//					ZipEntry e = new ZipEntry("log.txt");
+//					out.putNextEntry(e);
+//					out.closeEntry();
+//					out.close();
+//					String filePathLog = "log.zip";
+//					//ZipUtil.pack(new File(filePathLog), zippedLogFile);
+//					MimeBodyPart attachmentBodyPartLog = new MimeBodyPart();
+//					String fileNameLog = "log.zip";
+//
+//
+//					DataSource sourceLog = new FileDataSource(filePathLog);
+//					attachmentBodyPartLog.setDataHandler(new DataHandler(sourceLog));
+//					attachmentBodyPartLog.setFileName(fileNameLog);
+//
+//					// add to our message
+//					multipart.addBodyPart(attachmentBodyPartLog);
+//					System.out.println("Log File Attached");
+//				}
+//			}
 
 
             //Add message and attachments
@@ -214,13 +208,13 @@ class EmailResults extends BaseTest{
             System.out.println("Error Sending mail: ");
             e.printStackTrace();
         }
-		catch(FileNotFoundException e)
-		{
-			e.printStackTrace();
-		}
-		catch(IOException e)
-		{
-			e.printStackTrace();
-		}
+//		catch(FileNotFoundException e)
+//		{
+//			e.printStackTrace();
+//		}
+//		catch(IOException e)
+//		{
+//			e.printStackTrace();
+//		}
 	}
 }
